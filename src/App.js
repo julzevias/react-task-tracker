76eb60b1e2073ad01069a9c6adadbebe47d80879
Julie Yue <julie.yue.2011@gmail.com>
2021-09-04 10:54:27 -0500

    date

--- src/App.js
+++ src/App.js
@@ -1,70 +1,68 @@
-import { useState } from 'react'
-import Header from './components/Header' 
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
-
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
-  const [showAddTask, setShowAddTask] = useState
-  (false)
-  const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: 'Doc Appointment',
-            day: '2/5 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'school meeting',
-            day: '2/6 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food shopping',
-            day: '2/5 2:30pm',
-            reminder: false,
-        }
-    
-    ]
-)
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doc appointment",
+      parseday: "8/16/2021",
+      time: "9:00 AM",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "School meeting",
+      parseday: "9/14/2021",
+      time: "8:00 AM",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      parseday: "7/14/2021",
+      time: "3:00 PM",
+      reminder: false,
+    },
+  ]);
 
-//Add Task
-const addTask = (task) => {
- const id = Math.floor(Math.random() * 10000) + 1
- const newTask={id, ...task}
- setTasks([...tasks, newTask])
-}
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
-//Delete Task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
-
-}
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   //Toggle reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id 
-    ? { ...task, reminder: !task.reminder} : task))
-  }
-
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask 
-        (!showAddTask)} 
-        showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
-      <Tasks 
-        tasks={tasks} 
-        onDelete={deleteTask} 
-        onToggle={toggleReminder}
-        /> 
-      ): ('No Tasks To Show')}
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,54 +1,78 @@
-import { useState } from 'react'
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from "date-fns";
 
-const AddTask = ( {onAdd }) => {
-    const [text, setText] = useState('')
-    const [day, setDay] = useState('')
-    const [reminder, setReminder] = useState(false)
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [parseday, setParseDay] = useState("");
+  const [day, setDay] = useState(null);
+  const [time, setTime] = useState("");
+  const [reminder, setReminder] = useState(false);
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        if (!text) {
-            alert('Please Add Task')
-            return
-        }
-
-        onAdd({ text, day, reminder })
-
-        setText('')
-        setDay('')
-        setReminder(false)
+    if (!text) {
+      alert("Please Add Task");
+      return;
     }
-    
-    return (
-        <form className='add-form' onSubmit=
-        {onSubmit}>
-            <div className='form-control'>
-                <label>Task</label>
-                <input type='text' 
-                placeholder='Add Task'
-                value={text} onChange={(e) => 
-                setText(e.target.value)} />
-            </div>
-            <div className='form-control'>
-                <label>Day & Time</label>
-                <input type='text' placeholder='Add Day and Time' 
-                value={day} onChange={(e) => setDay(e.target.value)}/>
-            </div>
-            <div className='form-control form-control-check'>
-                <label>Set Reminder</label>
-                <input 
-                type='checkbox' 
-                checked={reminder}
-                value={reminder} 
-                onChange={(e) => 
-                setReminder(e.currentTarget.checked)}
-                />
-            </div>
 
-            <input type='submit' value='Save Task'className='btn btn-block'/>
-        </form>
-    )
-}
+    onAdd({ text, parseday, reminder });
 
-export default AddTask
+    setText("");
+    setParseDay("");
+    setTime("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+
+      <div className="form-control">
+        <label>Day</label>
+        <DatePicker
+          selected={day}
+          placeholder="Add Day"
+          onChange={(date) => {
+            setDay(date);
+            setParseDay(format(date, "dd/MM/yyyy"));
+          }}
+          dateFormat="dd/MM/yyyy"
+        />
+      </div>
+      <div className="form-control">
+        <label>Time</label>
+        <input
+          type="text"
+          placeholder="Add Time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+
+      <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
+  );
+};
+
+export default AddTask;
